e68a3dda283d0de52a3a24b03abebfd4c16e61ea
OlyaBorzynska <mykytlo@gmail.com>
2026-09-22 09:46:04 +0300

    add global styles and header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,30 @@
 import { NavLink } from "react-router-dom";
 
+import css from "./Header.module.css";
+
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/psychologists">Psychologists</NavLink>
-        <NavLink to="/favorites">Favorites</NavLink>
-      </nav>
+    <header className={css.header}>
+      <div className="container">
+        <NavLink to="/" className={css.logo}>
+          Psychologists
+        </NavLink>
+
+        <nav className={css.navigation}>
+          <NavLink to="/" className={css.navLink}>
+            Home
+          </NavLink>
+
+          <NavLink to="/psychologists" className={css.navLink}>
+            Psychologists
+          </NavLink>
+        </nav>
+
+        <div className={css.authActions}>
+          <button type="button">Log In</button>
+          <button type="button">Registration</button>
+        </div>
+      </div>
     </header>
   );
 };
